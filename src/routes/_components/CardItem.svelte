c30f317124de8b60b608428c1dd20206e8a41795
alexhowez <alexhowezdev@gmail.com>
2024-02-26 11:27:26 -0300

    Update CardItem.svelte

--- src/routes/_components/CardItem.svelte
+++ src/routes/_components/CardItem.svelte
@@ -7,5 +7,7 @@
 
 <div class="my-2 flex justify-between items-center">
   <p>{key}: {value}</p>
-  <button class="px-3 py-1 bg-red-500 text-white font-medium rounded-sm hover:bg-blue-700" on:click={() => copy(value)}>COPY</button>
+  <button class="px-3 py-1 bg-red-500 text-white font-medium rounded-sm hover:bg-blue-700" 
+  use:copy={value}
+  >COPY</button>
 </div>
